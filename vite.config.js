39117bc77f47d3b56f1f7048d9fe50473425d171
davidmodfyi <david@mod.fyi>
2025-05-03 21:25:58 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-server: {
-  proxy: {
-    '/api': {
-      target: 'https://api.featherstorefront.com',
-      changeOrigin: true,
-      secure: true,
-      rewrite: (path) => path // Don't rewrite/strip the path
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.featherstorefront.com',
+        changeOrigin: true,
+        secure: true,
+        // Don't rewrite paths - this is important
+        rewrite: (path) => path
+      },
     },
   },
-},
 });
