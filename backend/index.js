4d73a05371878098c2588079e5791a3d69de0790
davidmodfyi <david@mod.fyi>
2025-05-09 20:28:22 -0400

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ console.log('Starting Feather API server...');
 console.log('Environment:', process.env.NODE_ENV || 'development');
 
 const corsOptions = {
-  origin: ['https://www.featherstorefront.com', 'https://featherstorefront.com'],
+  origin: ['https://www.featherstorefront.com', 'https://featherstorefront.com', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
