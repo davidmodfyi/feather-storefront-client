e1db833ff629702a2850a0a7318ddba34d3cfa6b
davidmodfyi <david@mod.fyi>
2025-05-31 21:35:39 -0400

    Update main.jsx

--- src/distributors/oceanwave/main.jsx
+++ src/distributors/oceanwave/main.jsx
@@ -1,10 +1,18 @@
+// Template for src/distributors/[DISTRIBUTOR]/main.jsx
+// Copy this content to each distributor's main.jsx file
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// Get distributor info from global scope (set by index.html)
+const distributorSlug = window.FEATHER_DISTRIBUTOR || 'default';
+
+console.log(`Initializing Feather Storefront for: ${distributorSlug}`);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App distributorSlug={distributorSlug} />
   </React.StrictMode>
 );
